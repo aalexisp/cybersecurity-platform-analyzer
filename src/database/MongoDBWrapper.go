29111b08bdacd0703dec32c3d9a3b8b64c43e00b
ShimaSama <tineta000@gmail.com>
2022-02-05 14:00:47 +0100

    Fix add_role

--- src/database/MongoDBWrapper.go
+++ src/database/MongoDBWrapper.go
@@ -100,14 +100,13 @@ func Add_user(client *mongo.Client, path string){
 	}
 }
 
-func Add_role(client *mongo.Client, path string) { //path of the json file with the roles, we create the roles in the admin databse
+func Add_role(client *mongo.Client, path string, database string) { //path of the json file with the roles
 
 	docsPath, _ := filepath.Abs(path)
 	byteValues, err := ioutil.ReadFile(docsPath)
 	if err != nil{
 		log.Fatal(err) //file doesn't exist
 		return
-	//	return false
 	}
 	var Docs []Roles
 
@@ -115,14 +114,13 @@ func Add_role(client *mongo.Client, path string) { //path of the json file with 
 	if err != nil{
 		log.Fatal(err) //structure is bad
 		return
-	//	return false
 	}
 	for i := range Docs {
 		doc := Docs[i]
-	
+
 		var priv bson.A
-		for j := range doc.Privileges {//recorrer privilegios
-		
+		for j := range doc.Privileges { //traverse all privileges
+
 			var act bson.A
 			for p := range doc.Privileges[j].Actions{
 				act = append(act, doc.Privileges[j].Actions[p])
@@ -130,12 +128,18 @@ func Add_role(client *mongo.Client, path string) { //path of the json file with 
 			priv = append(priv, bson.M{"resource": bson.M{ "db": doc.Privileges[j].Resource.Db,"collection": doc.Privileges[j].Resource.Collection ,}, "actions": act ,})
 
 		}
+		if len(doc.Privileges) == 0{
+			priv = bson.A{} //no privileges
+		}
 		var rol bson.A
 		for t := range doc.Roles {
 			rol = append(rol, bson.M{"role": doc.Roles[t].Role, "db":doc.Roles[t].Db})
 		}
-	//	r := client.Database("admin").RunCommand(context.Background(), bson.D{{"createRole", "newRol"},	{"privileges", bson.A{ bson.M{ "resource": bson.M{"db": "Cluster0", "collection": "",},"actions": bson.A{"insert","dbStats","collStats","compact",},},}}, {"roles", bson.A{bson.M{"role": "readWrite", "db": "Cluster0",},},}})
-		r := client.Database("admin").RunCommand(context.Background(), bson.D{{"createRole", doc.CreateRole},{"privileges", priv}, {"roles", rol}})
+		if len(doc.Roles) == 0{
+			rol = bson.A{} //no inherited rules
+		}
+		//r := client.Database("admin").RunCommand(context.Background(), bson.D{{"createRole", "newRol"},	{"privileges", bson.A{ bson.M{ "resource": bson.M{"db": "Cluster0", "collection": "",},"actions": bson.A{"insert","dbStats","collStats","compact",},},}}, {"roles", bson.A{bson.M{"role": "readWrite", "db": "Cluster0",},},}})
+		r := client.Database(database).RunCommand(context.Background(), bson.D{{"createRole", doc.CreateRole},{"privileges", priv}, {"roles", rol, }})
 		if r.Err() != nil {
 			fmt.Println("error at runCommand")
 			panic(r.Err())
@@ -143,8 +147,8 @@ func Add_role(client *mongo.Client, path string) { //path of the json file with 
 		}else{
 			fmt.Printf("Role %s was created successfully\n ", doc.CreateRole)
 		}
+	}
 
-	}	
 }
 
 func Connect(portdomain string, user string, database string, localhost bool) *mongo.Client {
@@ -160,7 +164,7 @@ func Connect(portdomain string, user string, database string, localhost bool) *m
 	if(localhost == true){
 		credential := options.Credential{
 			Username: user,
-			Password: string(password),
+			Password: string(password),	
 		}
 		//check authentication
 
